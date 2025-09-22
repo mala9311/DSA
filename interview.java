b9e658c83b6155d1c628f4b4e2d0e51b89002d3f
mala9311 <malakumari9311@gmail.com>
2025-09-22 23:02:54 +0530

    Find Minimum and Maximum in Array

--- interview.java
+++ interview.java
@@ -1,6 +1,7 @@
 public class interview {
     public static void main(String[] args) {
         int n[]= {10,5,17,30,40};
+        // Find minimum element in Array
          int min  = n[0];
          for(int i =1; i<n.length;i++){
             if(min>n[i]){
@@ -8,6 +9,15 @@ public class interview {
             }
          }
          System.out.println(min);
+
+        // Find Maximum element in Array
+        int max = n[0];
+        for(int i = 1; i<n.length;i++){
+            if(max<n[i]){
+                max = n[i];
+            }
+        }
+        System.out.println(max);
     }
     
 }
