7a5c7bad48341fe40ee86ac5fd2ab8bde02623e6
mala9311 <malakumari9311@gmail.com>
2025-09-22 21:39:58 +0530

    array declaration and initialization

---
+++ Array_01.java
@@ -0,0 +1,17 @@
+public class Array_01{
+    public static void main(String[]args){
+        //How to create an array
+         int age[] = {21,22,23,24,25};
+
+         //How to access
+
+         System.out.println(age[2]);
+         System.out.println(age[3]);
+
+         //How to chamge
+
+         age[2] = 30;
+         System.out.println(age[2]);
+
+    }
+}
