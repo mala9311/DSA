7713ef51b96c741ce50c4078c9527b1280eed7e7
mala9311 <malakumari9311@gmail.com>
2025-09-22 22:28:49 +0530

    array declaration and initialization

--- Array_01.java
+++ Array_01.java
@@ -1,17 +1,46 @@
+import java.util.Scanner;
 public class Array_01{
     public static void main(String[]args){
         //How to create an array
-         int age[] = {21,22,23,24,25};
+       //  int age[] = {21,22,23,24,25};
 
          //How to access
 
-         System.out.println(age[2]);
-         System.out.println(age[3]);
+      //*** System.out.println(age[2]);
+      // ***   System.out.println(age[3]);
 
          //How to chamge
 
-         age[2] = 30;
-         System.out.println(age[2]);
+       //  age[2] = 30;
+        //  System.out.println(age[2]);
+        // for(int i = 0;i<5;i++){
+        //     System.out.println(age[i]);
+        // }
+        // System.out.println("for each");
+        // for(int i : age){
+        //     System.out.println(i);
+        // }
+     //   int age[] = new int[5];
+        //How to insert
+        // age[0] = 25;
+        // age[1] = 20;
+        // for(int i : age){
+        //     System.out.println(i);
+        // }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many students do you want to add =");
+        int size = sc.nextInt();
+
+        int age[] = new int[size];
+
+        for(int i = 0; i<size;i++){
+            age[i] = sc.nextInt();
+
+        }
+        System.out.println("Student list");
+        for(int i :age){
+            System.out.println(i);
+        }
 
     }
 }
