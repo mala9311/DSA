2957e5c755d285531c4a13203dcbf09a403b2f26
mala9311 <malakumari9311@gmail.com>
2025-09-23 15:01:44 +0530

    Reverse the array

--- Reverse.java
+++ Reverse.java
@@ -3,20 +3,27 @@ import java.util.Arrays;
 public class Reverse {
     public static void main(String[] args) {
         int number[] = {2,4,6,8,9,10};
+         int start = 0;
+       int end = number.length -1;
        //***********Using for loop**********
        
-       for(int i =number.length -1;i> 0;i--){
-        }
-         System.out.println("Reverse the Arrays :"  + Arrays.toString(number));
+    //    for(int i =number.length -1;i> 0;i--){
+    //     int temp = number[start];
+    //     number[start] = number[end];
+    //     number[end]= temp;
+       
+    // }
+    //      System.out.println("Reverse the Arrays :"  + Arrays.toString(number));
 
        // Using while loop
        
-       int start = 0;
-       int end = number.length -1;
+      
        while(start<end) {
         int temp = number[start];
-        end = number[start];
-        start = number[temp];
+        number[start] = number[end];
+        number[end]= temp;
+        start++;
+        end--;
        }
        System.out.println("Reverse the Arrays :" +Arrays.toString(number));
 
