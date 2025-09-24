c42c05a1beaa26cc0be2e0e955671d330910b935
mala9311 <malakumari9311@gmail.com>
2025-09-24 16:53:17 +0530

    Find index of n in number[]

---
+++ FindRankAndIndex.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class FindRankAndIndex {
+    public static void main(String[] args) {
+        int number[] = {1,3,69,6,9,88};
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("REnter the number: ");
+        int n =sc.nextInt();
+
+        //save the original index
+        int originalIndex = -1;
+        for(int i = 0;i<number.length;i++){
+            if(number[i] == n){
+                originalIndex = i;
+                break;
+            }
+        }
+    
+
+        // Sort array in ascending order
+        int sortedArray[] = number.clone(); // clone to preserve original
+        Arrays.sort(sortedArray);
+
+        boolean found = false;
+        for (int i = 0; i < sortedArray.length; i++) {
+            if (n == sortedArray[i]) {
+                int rankFromHighest = sortedArray.length - i;
+                System.out.println(n + " is the " + rankFromHighest + " highest element");
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println(n + " is not in the array");
+        } else {
+            System.out.println("Original index of " + n + " is: " + originalIndex);
+         
+    }
+    
+}}
--- inedx_element.java
+++ inedx_element.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
 public class inedx_element {
     public static void main(String[] args) {
-        int number[] = {1,3,4,5,2,7,8};
+        int number[] = {1,2,3,4,5,6,7};
        Scanner sc = new Scanner(System.in);
        System.out.println("Enter the number from 1 to 10 :"); 
        int n = sc.nextInt();
-       boolean found = false;
+    //    boolean found = false;
+    int index =1;
        for(int i = 0; i<number.length;i++){
         if(n == number[i]){
-            System.out.println("number is present at"+ i +" of your n" +n);
-            found = true;
+            // System.out.println("number is present at"+ i +" of your n" +n);
+            // found = true;
+            index = i;
             break;
 
-        }
+        }}
+        System.out.println("iNDEX of" + n +" is"+index);
         
-        }
-        if(!found){
-            System.out.println("not found "+ n);
-        }
+        // if(!found){
+        //     System.out.println("not found "+ n);
+        // }
+        sc.close();
        }
     }
     
