720f66b59859eb86d34312b26aa21a73818f52b0
mala9311 <malakumari9311@gmail.com>
2025-09-26 13:25:28 +0530

    Moore's Voting Algorithm

--- Majority_Element.java
+++ Majority_Element.java
@@ -2,38 +2,55 @@ import java.util.Arrays;
 
 public class Majority_Element {
     public static void main(String[] args) {
-        //  int n[] ={1,5,1,3,2,1,8,1,1};
-          // First Method
-    //    for(int i = 0; i< n.length; i++){
+         int number[] ={1,5,1,3,2,1,8,1,1};
+         int n = number.length;
+        //  First Method
+    //    for(int i = 0; i< n; i++){
     //       int fre = 0;
-    //         for(int j= 0; j< n.length;j++){
-    //             if(n[i] == n[j]){
+    //         for(int j= 0; j< n;j++){
+    //             if(number[i] == number[j]){
     //                 fre++;
     //                }
     //         }
-    //        if(fre > n.length/2){
-    //         System.out.println("MJ element" + n[i]);
+    //        if(fre > n/2){
+    //         System.out.println("MJ element" + number[i]);
     //         break;
     //        }
-    //      }
+        //  }
          // Second Method
-        int n[] ={1,2,3,2,1,4,2,2,2,1};
-        Arrays.sort(n);
-        int L = n.length;
-         int count = 1;
-        for(int i= 1;i<L;i++){
+     //   int n[] ={1,2,3,2,1,4,2,2,2,1};
+        // Arrays.sort(n);
+        // int L = n.length;
+        //  int count = 1;
+        // for(int i= 1;i<L;i++){
            
-            if(n[i] == n[i-1]){
-                count++;
+        //     if(n[i] == n[i-1]){
+        //         count++;
+        //     }
+        //     else{
+        //         count = 1;
+        //     }
+        //     if(count> L/2){
+        //         System.out.println("Majority Element " + n[i]);
+        //         break;
+        //     }
+        // }
+        // Third Method *Moore's Voting Algorithm
+            int count = 0;
+            int candidate = 0;
+            
+            for(int i = 0;i<n;i++){
+                if(count ==0){
+                    candidate = number[i];
+                }
+                if(candidate == number[i]){
+                    count++;
+                }
+                else{
+                    count--;
+                }
             }
-            else{
-                count = 1;
-            }
-            if(count> L/2){
-                System.out.println("Majority Element " + n[i]);
-                break;
-            }
-        }
-            }
+                System.out.println("MJ element " + candidate);
+             }
     
 }
