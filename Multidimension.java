57133fd647fc9c14cce1ae526f09154b948b17d0
mala9311 <malakumari9311@gmail.com>
2025-10-02 21:37:54 +0530

    MultiDimension

--- Multidimension.java
+++ Multidimension.java
@@ -12,7 +12,21 @@ public class Multidimension {
             {1 ,2 ,3},
             {4 ,5 ,6}
         };
+       // System.out.println(Arrays.deepToString(arr));
+       
+        // Print using Arrays.deepToString
         System.out.println(Arrays.deepToString(arr));
+
+        // Print in matrix form
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
 
       //  System.out.println(arr[][]);
     }
