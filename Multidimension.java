43037bb49bf6b13d121b5f00be9ae44371b3b6dd
mala9311 <malakumari9311@gmail.com>
2025-10-02 21:36:22 +0530

    MultiDimension

---
+++ Multidimension.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Multidimension {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][2];
+        /*
+         1 2 3
+         4 5 6 
+
+         */
+        int[][] arr = {
+            {1 ,2 ,3},
+            {4 ,5 ,6}
+        };
+        System.out.println(Arrays.deepToString(arr));
+
+      //  System.out.println(arr[][]);
+    }
+    
+}
