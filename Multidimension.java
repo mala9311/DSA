0714d6114b68b1cd76973a5b686254a0e9dad632
mala9311 <malakumari9311@gmail.com>
2025-10-06 16:57:02 +0530

    MultiDimension

--- Multidimension.java
+++ Multidimension.java
@@ -1,34 +1,41 @@
+import java.util.Scanner;
 import java.util.Arrays;
 
 public class Multidimension {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int[][] arr = new int[3][2];
         /*
          1 2 3
          4 5 6 
 
          */
-        int[][] arr = {
-            {1 ,2 ,3},
-            {4 ,5 ,6}
-        };
+        // int[][] arr = {
+        //     {1 ,2 ,3},
+        //     {4 ,5 ,6}
+        // };
        // System.out.println(Arrays.deepToString(arr));
        
         // Print using Arrays.deepToString
-        System.out.println(Arrays.deepToString(arr));
-
+      //  System.out.println(Arrays.deepToString(arr));
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = sc.nextInt();
+            }}
         // Print in matrix form
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
             }
+            
             System.out.println();
         }
     }
-}
+        // System.out.println("Using Arrays.deepToString:");
+       // System.out.println(Arrays.deepToString(arr));
 
 
       //  System.out.println(arr[][]);
     }
     
-}
+
