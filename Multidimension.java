ed9b99bab26c105d27df28876d65326bfa376a7e
mala9311 <malakumari9311@gmail.com>
2025-10-07 13:54:11 +0530

    MultiDimension

--- Multidimension.java
+++ Multidimension.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 public class Multidimension {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] arr = new int[3][2];
+        int[][] arr = new int[3][3];
+        System.out.println("Enter the Matrix of 3,3");
         /*
          1 2 3
          4 5 6 
@@ -24,12 +25,14 @@ public class Multidimension {
             }}
         // Print in matrix form
         for (int row = 0; row < arr.length; row++) {
-            for (int col = 0; col < arr[row].length; col++) {
-                System.out.print(arr[row][col] + " ");
-            }
+            // for (int col = 0; col < arr[row].length; col++) {
+            //     System.out.print(arr[row][col] + " ");
+            // }
             
-            System.out.println();
+            // System.out.println();
+            System.out.println(Arrays.toString(arr[row]));
         }
+        
     }
         // System.out.println("Using Arrays.deepToString:");
        // System.out.println(Arrays.deepToString(arr));
